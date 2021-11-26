a5d85bfab5f1fd9b72ce6c46daea7a2dce18de5e
aquiwer <70368630+aquiwer@users.noreply.github.com>
2021-11-26 19:05:13 +0100

    Some fixes

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import asyncio
 from aiogram import Bot, Dispatcher, executor, types
-import requests
-# from bs4 import BeautifulSoup
-import re
-import math
-import time
-import functools
 
-# import numpy as np
-
-bot = Bot(token="2123107411:AAEcsw9wKqG7B_TFHPMr5brQC3Rozqbu6Q4")
+bot = Bot(token="your token")
 dp = Dispatcher(bot)
 
 
@@ -25,11 +16,7 @@ async def check_language(message: types.Message):
     await message.reply("Привет :D")
 
 
-#
-# @dp.message_handler(content_types=["sticker"])
-# async def delete_stickers(message: types.Message):
-#     if message["from"].username == "dimovski5":
-#         await bot.delete_message(message.chat.id, message.message_id)
+
 
 
 executor.start_polling(dp, skip_updates=True)
